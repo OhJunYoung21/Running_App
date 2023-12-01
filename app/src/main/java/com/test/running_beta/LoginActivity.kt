2d112b7f69366d265433efcd0a1c9f25aa6694f1
JUNYONG <dhwnsdyd21@gmail.com>
2023-12-01 17:35:15 +0900

    login_UI commit

--- app/src/main/java/com/test/running_beta/LoginActivity.kt
+++ app/src/main/java/com/test/running_beta/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.test.running_beta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.running_beta.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+
     }
 }

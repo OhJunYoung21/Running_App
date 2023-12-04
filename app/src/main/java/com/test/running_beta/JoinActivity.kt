a2472754e678a8ec95c957e103ff71dbd83a4b0d
JUNYONG <dhwnsdyd21@gmail.com>
2023-12-04 15:49:26 +0900

    database_check commit

--- app/src/main/java/com/test/running_beta/JoinActivity.kt
+++ app/src/main/java/com/test/running_beta/JoinActivity.kt
@@ -20,6 +20,8 @@ class JoinActivity : AppCompatActivity() {
     lateinit var id: String
     lateinit var password: String
 
+    lateinit var gender :String
+
     lateinit var db: AppDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,59 +41,37 @@ class JoinActivity : AppCompatActivity() {
 
         supportActionBar?.setTitle(" ")
 
-        //runOnUiThread{ Toast.makeText(this, "비밀번호가 서로 일치 하지 않습니다..", Toast.LENGTH_SHORT).show() }
-
         //late init 변수에 값 할당
-        name = binding.userName.toString()
+        name = binding.userName.text.toString()
 
-        number = binding.userNumber.toString()
+        number = binding.userNumber.text.toString()
 
-        id = binding.userId.toString()
+        id = binding.userId.text.toString()
 
-        password = binding.userPw.toString()
+        password = binding.userPw.text.toString()
 
-        binding.checkId.setOnClickListener {
+        gender = ""
 
-            val checkThread = Runnable {
-
-                //아이디 유효성 검사 완료
-
-                if(db.getUserDAO().getIdList().contains(id)){
-
-                    runOnUiThread { Toast.makeText(this,"다른 사용자가 사용중인 아이디입니다.",Toast.LENGTH_SHORT).show() }
-
-                }
-
-
-            }
-            val thread = Thread(checkThread)
-
-            thread.start()
-
-
-        }
-
-
-
-        //아이디 중복확인 버튼 클릭시 발생하는 이벤트 처리
         binding.joinBtn.setOnClickListener {
 
-            //db에 접근하는 작업은 새로운 Thread에서 실행해줘야 Main Thread에 과부하가 걸리지 않는다.
+            val joinThread = Runnable {
 
-            val newThread = Runnable {
-
-                db.getUserDAO().insertUser(UserEntity(name = name, phoneNumber = number,id = id,password=password,gender = " "))
-
-                runOnUiThread { Toast.makeText(this,"회원가입이 완료되었습니다.",Toast.LENGTH_SHORT).show() }
+                db.getUserDAO().insertUser(UserEntity(name=name, phoneNumber = number,id = id, password = password,gender = ""))
 
             }
 
-
-            val thread = Thread(newThread)
+            val thread = Thread(joinThread)
 
             thread.start()
 
+
         }
+
+
+
+
+
+
 
 
     }

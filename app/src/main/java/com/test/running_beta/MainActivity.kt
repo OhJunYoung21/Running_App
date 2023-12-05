33b11cd5b84444dda6c6e1c2abde5fec0503cc8b
JUNYONG <dhwnsdyd21@gmail.com>
2023-12-05 16:10:02 +0900

    main UI commit

--- app/src/main/java/com/test/running_beta/MainActivity.kt
+++ app/src/main/java/com/test/running_beta/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.test.running_beta
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this,LoginActivity::class.java)
+        if(isLoggedIn()){
 
-        startActivity(intent)
+            val intent = Intent(this,JoinActivity::class.java)
+            startActivity(intent)
 
+        }else{
+
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+    fun isLoggedIn(): Boolean {
+        val preference = this.getSharedPreferences("login_pref", Context.MODE_PRIVATE)
+        //default 값 으로는 false 를 리턴 한다.
+        return preference.getBoolean("isLoggedIn", false)
     }
 }

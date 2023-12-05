45a017144224decba63a58f13a967d4efd6adb3c
JUNYONG <dhwnsdyd21@gmail.com>
2023-12-05 20:37:01 +0900

    main UI commit

--- app/src/main/java/com/test/running_beta/MainActivity.kt
+++ app/src/main/java/com/test/running_beta/MainActivity.kt
@@ -5,29 +5,60 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.test.running_beta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(isLoggedIn()){
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-            val intent = Intent(this,JoinActivity::class.java)
-            startActivity(intent)
+        //로그인 된 상태가 아니라면 로그인 화면으로 이동
+        if (!isLoggedIn()) {
 
-        }else{
+            val intent = Intent(this, LoginActivity::class.java)
 
-            val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
 
         }
+
+        binding.logout.setOnClickListener {
+
+            Toast.makeText(this,"로그아웃 되었습니다.",Toast.LENGTH_SHORT).show()
+
+        }
     }
+
     fun isLoggedIn(): Boolean {
         val preference = this.getSharedPreferences("login_pref", Context.MODE_PRIVATE)
         //default 값 으로는 false 를 리턴 한다.
         return preference.getBoolean("isLoggedIn", false)
+    }
+
+    fun logOut() {
+
+        saveLoginStatus(false)
+        saveLoggedInId("")
+
+    }
+
+    fun saveLoginStatus(loggedIn: Boolean) {
+        val preference = this.getSharedPreferences("login_pref", Context.MODE_PRIVATE)
+        val editor = preference.edit()
+        editor.putBoolean("isLoggedIn", loggedIn)
+        editor.apply()
+    }
+
+    fun saveLoggedInId(userId: String) {
+        val sharedPreferences = this.getSharedPreferences("login_pref", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("loggedInUserId", userId)
+        editor.apply()
     }
 }

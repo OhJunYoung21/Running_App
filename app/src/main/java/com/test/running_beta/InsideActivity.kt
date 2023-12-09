578d75a20c6f7b30b1cd12ee9cc6b1ceb8c14950
JUNYONG <dhwnsdyd21@gmail.com>
2023-12-09 11:19:51 +0900

    Location Marker commit

--- app/src/main/java/com/test/running_beta/InsideActivity.kt
+++ app/src/main/java/com/test/running_beta/InsideActivity.kt
@@ -3,6 +3,7 @@ package com.test.running_beta
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
@@ -18,11 +19,13 @@ import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 import com.test.running_beta.databinding.ActivityInsideBinding
 
 class InsideActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -32,6 +35,10 @@ class InsideActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityInsideBinding
 
     private lateinit var mapview: MapView
+
+    private lateinit var locationSource: FusedLocationSource
+
+    private val marker = Marker()
 
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient //자동으로 gps값을 받아온다.
     lateinit var locationCallback: LocationCallback //gps응답 값을 가져온다.
@@ -50,12 +57,11 @@ class InsideActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         mapview = binding.mapView
-        //사용자 경험을 위해 만들어진 bundle 객체입니다. 만일
-        mapview.getMapAsync(this)
 
         // 위치 권한이 승인된 경우, if 문 안의 명령문 수행
         if (checkLocationPermissions()) {
-            initLocation()
+            //비동기적으로 onMapReady() 콜백함수를 실행하는 명령문
+            mapview.getMapAsync(this)
         } else {
             //위치 권한이 승인 되지 않은 경우, 권한 요청 작업 수행(정상 작동 확인)
             requestLocationPermissions()
@@ -63,13 +69,29 @@ class InsideActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(naverMap: NaverMap) {
 
-        var camPos = CameraPosition(
-            LatLng(10.38, 10.55),
-            9.0
-        )
-        naverMap.cameraPosition = camPos
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // 위치 정보 요청
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                // 위치를 받아와서 지도의 초기 위치로 설정
+                val camPos = CameraPosition(
+                    LatLng(it.latitude, it.longitude),
+                    9.0
+                )
+
+                marker.position = LatLng(it.latitude, it.longitude)
+
+                marker.map = naverMap
+
+                naverMap.cameraPosition = camPos
+            }
+        }
+        naverMap.uiSettings.isLocationButtonEnabled = true
+
         this.naverMap = naverMap
 
     }
@@ -86,49 +108,5 @@ class InsideActivity : AppCompatActivity(), OnMapReadyCallback {
         ActivityCompat.requestPermissions(this, permissions, 123)
     }
 
-    @SuppressLint("MissingPermission")
-    private fun initLocation() {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
-            location?.let {
-                // 위치를 지도의 초기 위치로 설정
-                val camPos = CameraPosition(
-                    LatLng(it.latitude, it.longitude),
-                    9.0
-                )
-
-                naverMap.cameraPosition = camPos
-            }
-        }
-
-        // 위치 변경 시마다 호출되는 콜백 등록
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                locationResult?.lastLocation?.let {
-                    // 위치가 변경되면 지도의 카메라를 업데이트
-                    val camUpdate = CameraUpdate.scrollTo(LatLng(it.latitude, it.longitude))
-                    naverMap.moveCamera(camUpdate)
-                }
-            }
-        }
-
-        // 위치 업데이트를 요청
-        startLocationUpdates()
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun startLocationUpdates() {
-        val locationRequest = LocationRequest.create()
-            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-            .setInterval(500)
-            .setFastestInterval(200)
-
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            Looper.getMainLooper()
-        )
-    }
 
 }

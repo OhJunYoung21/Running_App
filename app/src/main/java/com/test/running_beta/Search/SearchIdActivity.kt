b99fbf714cf235f675581de5c9ba341770de91d7
JUNYONG <dhwnsdyd21@gmail.com>
2023-12-15 13:35:24 +0900

    Search commit

---
+++ app/src/main/java/com/test/running_beta/Search/SearchIdActivity.kt
@@ -0,0 +1,12 @@
+package com.test.running_beta.Search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.running_beta.R
+
+class SearchIdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_id)
+    }
+}
